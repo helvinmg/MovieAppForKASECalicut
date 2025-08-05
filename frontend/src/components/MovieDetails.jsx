5372c5453126ee05e7a88ce4fadfda22a45f2402
helvinmg <helvin.m.g@gmail.com>
2025-08-05 16:33:59 +0530

    last changes

--- frontend/src/components/MovieDetails.jsx
+++ frontend/src/components/MovieDetails.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 function MovieDetails() {
+    const [movies, setMovies] = useState([]);
     useEffect(()=>{
         fetch('http://localhost:3333/movies/all').
         then(resp=>resp.json()).
-        then(data=>console.log(data))
+        then(data=>setMovies(data))
     },[])
   return (
     <>
@@ -11,7 +12,9 @@ function MovieDetails() {
         <thead>
         <tr><th>Movie Id</th><th>Movie Name</th><th>Movie Description</th><th>Movie Rating</th></tr>
         </thead>
-
+        <tbody>
+            {movies.map(mov=>(<tr><td>{mov.movieid}</td><td>{mov.moviename}</td><td>{mov.moviedescription}</td><td>{mov.movierating}</td></tr>))}
+        </tbody>
       </table>
     </>
   )
